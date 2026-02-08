59b1b4a42e112f075e604a9f4bb0055d2e04de53
SUPERTKY <purple_chips@outlook.jp>
2026-02-08 16:06:46 +0900

    Update main.js

--- main.js
+++ main.js
@@ -245,8 +245,8 @@ clawRMesh.scale.setScalar(WORLD_SCALE * ARM_SCALE);
 
   clawPivot.position.set(0.0, -1, 0.0); // ←「先端」のつもりの仮置き
 
-  clawLMesh.position.set(2.5, 0.0, 0.0);
-  clawRMesh.position.set(-2.5, 0.0, 0.0);
+  clawLMesh.position.set(0.0, 0.0, 1);
+  clawRMesh.position.set(0.0, 0.0, -1);
 
   // もし左右が逆/向きが違うなら回転で合わせる
   // clawLMesh.rotation.y = Math.PI;

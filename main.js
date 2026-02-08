26324615c7f0b108d27615c40c8e4fb37991c47c
SUPERTKY <purple_chips@outlook.jp>
2026-02-08 19:56:52 +0900

    Update main.js

--- main.js
+++ main.js
@@ -459,7 +459,7 @@ addDebugDotLocal(clawPivot, hingeR_local, 0.03);
 
 // ★爪の原点がヒンジに無い場合の補正（要調整）
 clawLMesh.position.set(0, -1.95, -0.2);
-clawRMesh.position.set(0, -1.7, -0.2);
+clawRMesh.position.set(0, -1.8, -0.2);
 
 // 置き場所（左上）
 armGroup.position.set(-1.2, 1.6, 0.6);

5e49162b4fba8d1d9987067b8b14aa85e2e0eb09
SUPERTKY <purple_chips@outlook.jp>
2026-02-08 16:55:47 +0900

    Update main.js

--- main.js
+++ main.js
@@ -253,8 +253,8 @@ clawPivot.add(clawLPivot);
 clawPivot.add(clawRPivot);
 
 // ★ヒンジ位置（要調整）
-clawLPivot.position.set(0, -0.5, 0.8);
-clawRPivot.position.set(0, -0.5, -0.8);
+clawLPivot.position.set(0, -1.3, 0.8);
+clawRPivot.position.set(0, -1.3, -0.8);
 
 // ===== 爪メッシュは「ピボットの子」 =====
 clawLPivot.add(clawLMesh);

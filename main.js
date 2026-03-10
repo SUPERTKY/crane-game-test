7e7c6ba9c38ee00e656c300608213a3217484856
SUPERTKY <purple_chips@outlook.jp>
2026-03-10 19:32:52 +0900

    Update main.js

--- main.js
+++ main.js
@@ -270,7 +270,7 @@ function quatFromEuler(x, y, z) {
 }
 
 const CLAW_R_CLOSED = -0.2;
-const CLAW_R_OPEN   = 0.05;
+const CLAW_R_OPEN   = 0.3;
 // ===== 自動シーケンス設定 =====
 const CLAW_OPEN_TIME = 0.6;   // 開くのにかける秒
 const ARM_DROP_DIST  = 1.0;  // 下げる距離（Y方向）
